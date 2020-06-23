e6e2793e26fb116a5ef84d78d9bb5fdaa657feaf
s18727 <61729768+s18727@users.noreply.github.com>
2020-06-23 11:25:15 +0200

    Add files via upload

---
+++ kolPoprawa/DTO/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kolokwium2.DTO
+{
+    public class Project
+    {
+        public int IdTeam { get; set; }
+        public int idTask { get; set; }
+        public String name { get; set; }
+        public DateTime deadline
+        {
+            get; set;
+        }
+    }
+}
---
+++ kolPoprawa/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kolPoprawa.Models
+{
+    public class Project
+    {
+        public int IdTeam { get; set; }
+        public String Name { get; set; }
+        public DateTime date { get; set; }
+    }
+}
